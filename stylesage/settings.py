6e83687c672683f90cd0cf266335821922f19da9
Raja Haseeb Fayyaz <rajahaseebfayyaz@gmail.com>
2024-09-28 11:17:17 +0000

    fix allauth

--- stylesage/settings.py
+++ stylesage/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'stylesage.urls'
